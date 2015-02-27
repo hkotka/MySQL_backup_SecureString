a26dc1d0047955369557ee7e0b589b153875451a
Henri Kotka <henri.kotka@gmail.com>
2015-02-27 09:28:01 +0200

    Initail commit
    Initial commit

---
+++ MySQL_backup_SecureString.ps1
@@ -0,0 +1,13 @@
+﻿$hash = "SecureString-has goes here"
+$password = ConvertTo-SecureString -string $hash
+$user = "backup"
+
+$cred = New-Object System.Net.NetworkCredential($user, $password, "")
+
+$date = Get-Date
+$dateString = $date.Year.ToString() + "-" + $date.Month.ToString() + "-" + $date.Day.ToString()
+$backupDest = "C:\backups\MySQL\mysql_backup_"+ $dateString + ".sql"
+$mysqldumpLocation = ".\mysqldump.exe"
+$execute = $mysqldumpLocation+" -u"+$cred.UserName+" -p"+$cred.Password +" --all-databases > " + $backupDest
+
+invoke-expression $execute
