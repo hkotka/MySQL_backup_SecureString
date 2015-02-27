827cc103f349c4a8e749f58b75dead8d2a61c20d
Henri Kotka <henri.kotka@gmail.com>
2015-02-27 11:52:54 +0200

    Minor changes in powershell printed text

--- create_secure_password.ps1
+++ create_secure_password.ps1
@@ -1,5 +1,8 @@
 $password = read-host -prompt "Enter password to be secured"
-write-host "Secure password hash is:" $password
 $hash = ConvertTo-SecureString $password -force -asPlainText
+
 $data = ConvertFrom-SecureString $hash
+write-host "Password to be encrypted is:" $password
+write-host "Secured hash is:" $data
+
 $data | out-file .\secure_pw.txt
